5e7b622b7cf9ee5775f4ae37672055e6a4cbb703
dasha <dashavashunina@gmail.com>
2024-10-04 22:35:46 +0100

    Add rna_dna.py

---
+++ add_modules/rna_dna.py
@@ -0,0 +1,69 @@
+def is_dna(string):
+    dna = {'a', 't', 'c', 'g'}
+    return set(string.lower()).issubset(dna)
+
+
+def is_rna(string):
+    rna = {'a', 'u', 'c', 'g'}
+    return set(string.lower()).issubset(rna)
+
+dna_complement = {'A': 'T',
+                  'a': 't',
+                  'C': 'G',
+                  'c': 'g',
+                  'T': 'A',
+                  't': 'a',
+                  'G': 'C',
+                  'g': 'c'}
+rna_complement = {'A': 'T',
+                  'a': 't',
+                  'C': 'G',
+                  'c': 'g',
+                  'U': 'A',
+                  'u': 'a',
+                  'G': 'C',
+                  'g': 'c'}
+dna_transcribe = {'A': 'A',
+                  'a': 'a',
+                  'C': 'C',
+                  'c': 'c',
+                  'T': 'U',
+                  't': 'u',
+                  'G': 'G',
+                  'g': 'g'}
+
+
+def reverse(string):
+    if is_dna(string) or is_rna(string):
+        return string[::-1]
+    else:
+        return 'is_not_NA'
+
+
+def complement(string):
+    comp = ''
+    if is_dna(string):
+        for i in string:
+            comp += dna_complement[i]
+    if is_rna(string):
+        for i in string:
+            comp += rna_complement[i]
+    if is_dna(string) is False and is_rna(string) is False:
+        comp = 'is_not_NA'
+    return comp
+
+
+def reverse_complement(string):
+    return complement(reverse(string))
+
+
+def transcribe(string):
+    transcript = ''
+    if is_dna(string):
+        for i in string:
+            transcript += dna_transcribe[i]
+    if is_rna(string):
+        transcript = 'it_is_RNA'
+    if is_dna(string) is False and is_rna(string) is False:
+        transcript = 'is_not_NA'
+    return transcript
