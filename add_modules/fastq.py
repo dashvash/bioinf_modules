aa67a95a49dc89b7d70880cd6a0ef22834db1360
dasha <dashavashunina@gmail.com>
2024-10-11 10:41:15 +0000

    Add new function read_fastq to add_modules/fastq.py

--- add_modules/fastq.py
+++ add_modules/fastq.py
@@ -9,3 +9,12 @@ def quality_threshold(qstring: str):
     for q in qstring:
         total += ord(q)
     return total/len(qstring) - 33
+
+
+def read_fastq(file_path):
+    with open(file_path, 'r') as file:
+        seqs = {}
+        lines = file.readlines()
+        for num in range(0,len(lines),4):
+            seqs[lines[num].rstrip()] = (lines[num+1].rstrip(),lines[num+3].rstrip())
+    return seqs
